21274ce575871596a88a85bfab3919a2ff35a3a8
Vardaan <vardaan.agarwal.98@gmail.com>
2021-11-06 00:54:31 +0530

    add: navbar logo

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../App.css";
+function Navbar() {
+  return (
+    <div className="nav-bar">
+      <div className="nav-header">
+        <img className="nav-logo-img" src="logo.png" alt="logo" />
+        <span className="logo-name">Vardaan Agarwal</span>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
