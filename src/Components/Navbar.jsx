9606a3f3dfa7755d536b518d49c444ac0a89f104
Vardaan <vardaan.agarwal.98@gmail.com>
2021-11-06 21:52:55 +0530

    add : tailwind css

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,12 +1,26 @@
 import React from "react";
-import "../App.css";
+
 function Navbar() {
   return (
-    <div className="nav-bar">
-      <div className="nav-header">
-        <img className="nav-logo-img" src="logo.png" alt="logo" />
-        <span className="logo-name">Vardaan Agarwal</span>
-      </div>
+    <div className="w-full h-1/6 py-1/6 bg-orange-900 flex flex-row justify-between">
+      <span className="text-3xl p-5 text-white">Vardaan Agarwal</span>
+      <ul className="mt-5 text-white">
+        <li className="p-3">
+          <a href="/">About Me</a>
+        </li>
+        <li className="p-3">
+          <a href="/">Skills</a>
+        </li>
+        <li className="p-3">
+          <a href="/">Projects</a>
+        </li>
+        <li className="p-3">
+          <a href="/">Blogs</a>
+        </li>
+        <li className="p-3">
+          <a href="/">Contact</a>
+        </li>
+      </ul>
     </div>
   );
 }
